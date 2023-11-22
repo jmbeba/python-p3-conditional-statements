4b86d3e9badc0a86de7e1c8ad1a42ca52c2e8f40
John Johnson <jjmbeba@gmail.com>
2023-11-22 21:08:06 +0300

    lab completed

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,44 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if(username.lower() == 'admin' and password == '12345'):
+        return 'Access granted'
+    else:
+        return 'Access denied'
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if(temperature > 85):
+        return "It's too dang hot out there!"
+    if(temperature > 40 and temperature < 65):
+        return "It's a little chilly out there!"
+    if(temperature < 40):
+        return "It's brisk out there!"
+    else:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if(num % 3 == 0 and num % 5 == 0):
+        return "FizzBuzz"
+    elif(num % 3 == 0):
+        return "Fizz"
+    elif(num % 5 == 0):
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if(operation == '+' or operation == '-' or operation == '*' or operation == '/'):
+        calc_map = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*":num1 * num2,
+        "/": num1 / num2
+        }
+    
+        return calc_map.get(operation)
+    else:
+        print("Invalid operation!")
+        return None
